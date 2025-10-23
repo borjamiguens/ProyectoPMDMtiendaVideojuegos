4a6a5d1ffd082e1ad74b2b1c189afecad240c96c
Angel Rodriguez Kassai <a23angelrk@teis.intranet>
2025-10-23 18:32:47 +0200

    Componente BottomBarButtonComponent

---
+++ app/src/main/java/com/example/proyectopmdmtiendavideojuegos/myComponents/BottomBarButtonComponent.kt
@@ -0,0 +1,59 @@
+package com.example.proyectopmdmtiendavideojuegos.myComponents
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.proyectopmdmtiendavideojuegos.R
+
+
+@Composable
+fun BottomBarButton(
+    image: Int,
+    text: String,
+    modifier: Modifier = Modifier,
+    action: () -> Unit
+) {
+    Column (
+        modifier = modifier
+            .clickable { action() },
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        Image(
+            painter = painterResource(id = image),
+            contentDescription = "",
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+
+        Text(
+            text = text,
+            color = Color.White,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun BottomBarButtonPreview () {
+    BottomBarButton(image = R.drawable.a, text = "CATEGORIAS") { }
+
+}

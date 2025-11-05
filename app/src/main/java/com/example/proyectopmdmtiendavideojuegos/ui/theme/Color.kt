d74eef44a4f1e240bc5d3f18e21bfd4c88079602
borja <borja@example.com>
2025-11-05 14:08:25 +0100

    anadiendo mods

--- app/src/main/java/com/example/proyectopmdmtiendavideojuegos/ui/theme/Color.kt
+++ app/src/main/java/com/example/proyectopmdmtiendavideojuegos/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//colores al estilo Seattle Supersonics 90
+
+val SonicGreen = Color(0xFF005736)
+val SonicGold = Color(0xFFFFC72C)
+val SonicRed = Color(0xFFD50032)
+val SonicCream = Color(0xFFF5F5DC)
+val SonicBlack = Color(0xFF121212)
